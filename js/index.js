0562060e8d8fd83dc3780fef267b7a747c873ba2
gomez-fernando <ferdgomez@hotmail.es>
2022-05-22 15:00:50 +0200

    Add first render of game in html version

--- js/index.js
+++ js/index.js
@@ -6,8 +6,10 @@ let canvas = document.querySelector('#canvas');
 let canvasWidth = 800;
 let canvasHeight = 800;
 
+let reloadButton = document.querySelector('#reload')
 
 const game = () => {
+   
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
 
@@ -20,7 +22,18 @@ const game = () => {
         console.log(array);
     }, 10 );
 
+   
+
+
+    
+    reloadButton.addEventListener('click', function(){location.reload()});
+
+    
+        
+
 }
+
+
 
 // IIFE
 (() => {
